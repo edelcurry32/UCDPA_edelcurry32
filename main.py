da2c1474793d4950d5a41917c66fe1f5c3ce03f5
Edel Curry <edelcurry32@gmail.com>
2021-04-11 16:00:05 +0100

    "second commit"

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-a = [1,2,3,4,5]
-print(a[::2])
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-b= [10,20,30,40,50]
-print(1:4)
+data = pd.read_csv(r"2019.csv")
+
+#print (data.columns)
+
+x = data['Country or region'].head(10)
+h = data['Score'].head(10)
+c = ["red", "green"]
+plt.bar(x,h,width =0.5, color = "c")
+plt.xlabel("Country")
+plt.ylabel("Score")
+plt.title("2019 Happiest Countries")
+plt.show()
