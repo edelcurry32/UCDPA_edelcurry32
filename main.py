2e0183d80f499042748d1f71bd28f11cd6a94eeb
Edel Curry <edelcurry32@gmail.com>
2021-04-13 22:43:28 +0100

    "commit 13/04/2021"

--- main.py
+++ main.py
@@ -8,26 +8,11 @@ data = pd.read_csv(r"2019.csv")
 
 #print (data.column s)
 
-palette = sns.color_palette("bright")
-sns.palplot(palette)
+x=data['GDP per capita']
+y=data['Score']
 
-fig= plt.figure(figsize=(15,8))
-g=gs.GridSpec(ncols=1, nrows=2, figure=fig)
-plt.suptitle("World's Happiness Index 2019", family='Calibri', weight='bold', size=16)
-ax1=plt.subplot(g[0,0])
+sns.scatterplot(x=data['GDP per capita'], y=data['Score'])
 
-bot_10= data.tail(10)
-top_10=data.head(10)
-
-x1=sns.barplot(data=top_10, x=top_10 ['Score'],y=top_10 ['Country or region'])
-#x1.set_xlabel('')
-x1.xaxis.set_visible(False)
-x1.annotate("Top 10 Happiest Countries",xy=(8,2), weight='bold', size=9, family='Calibri',)
-x2=plt.subplot(g[1,0], sharex=ax1)
-x2=sns.barplot(data=bot_10, x=bot_10['Score'],y=bot_10 ['Country or region'])
-x2.annotate("10 Least Happiest Countries",xy=(8,2), weight='bold', size=9, family='Calibri',)
-for s in ['left','right','top','bottom']:
-    x1.spines[s].set_visible(False)
-    x2.spines[s].set_visible(False)
+sns.regplot(x=data['GDP per capita'], y=data['Score'])
 
 plt.show()
