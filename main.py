8fa231b08615ce9e9c489303f8ab4824fd570296
Edel Curry <edelcurry32@gmail.com>
2021-04-18 18:36:18 +0100

    Data Cleaning

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ data = pd.read_csv(r"2019.csv")
 
 #print (data.columns)
 
-seq_list = [1,2,3,4,5]
-print(list(reversed(seq_list)))
+import pandas as pd
+import matplotlib.gridspec as gs
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-x = data['Country or region'].head(10)
-h = data['Score'].head(10)
-c = ["blue"]
-plt.bar(x,h,width =0.6, linewidth =20, edgecolor = "yellow", color = "blue")
-plt.xlabel("Country")
-plt.ylabel("Score")
-plt.title("2019 Happiest Countries")
+data = pd.read_csv(r"2019.csv")
+
+#print (data.column s)
+
+x=data['Generosity']
+y=data['Score']
+
+sns.scatterplot(x=data['Generosity'], y=data['Score'])
+
+sns.regplot(x=data['Generosity'], y=data['Score'])
+
 plt.show()
-
 
