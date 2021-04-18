d44012d0b9ee8c05425e6859a939e8ee65041749
Edel Curry <edelcurry32@gmail.com>
2021-04-18 14:45:20 +0100

    Data Cleaning

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import pandas as pd
-import matplotlib.gridspec as gs
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 
-data = pd.read_csv(r"2019.csv")
+data= pd.read_csv(r"2019.csv")
 
-#print (data.column s)
+print(data.head())
+print(data.shape)
 
-x=data['GDP per capita']
-y=data['Score']
-
-sns.scatterplot(x=data['GDP per capita'], y=data['Score'])
-
-sns.regplot(x=data['GDP per capita'], y=data['Score'])
-
-plt.show()
